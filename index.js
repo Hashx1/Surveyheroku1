6c967f0dcaa0997352e9faf156295c1862cd9506
unknown <aeoulsb@gmail.com>
2021-01-10 01:16:54 +0530

    oauth completed

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
   const express=require("express");
   const app=express();
+  const passport=reqpire("passport");
+  const GoogleStrategy=require('Passport-google-oauth20').Strategy;
+  const keys=require('./Config/keys');
 
-  app.get("/",(req,res)=>{
-    res.send("Konichiva and screw you das");
-  })
+  passport.use(new GoogleStrategy({
+    clientID:keys.googleClientID,
+    clientSecret:keys.googleClientSecret,
+    callbackURL:'/auth/google/callback'
+  },(accesToken)=>{
+    console.log(accessToken);
+  }
+  ));
 
   const PORT=process.env.PORT|| 5000;
   app.listen(PORT);
