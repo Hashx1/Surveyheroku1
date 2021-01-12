ed7ec44e694e57278aca8eef729aa1a9820ccf1d
unknown <aeoulsb@gmail.com>
2021-01-12 07:53:52 +0530

    Adding caps

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const keys = require('./config/keys');
+const keys = require('./Config/keys');
 const passport = require('passport');
-require('./model/user');
-require('./services/passport');
+require('./Model/user');
+require('./Services/passport');
 const cookieSession=require('cookie-session');
 mongoose.connect(keys.mongoURI);
 
@@ -18,7 +18,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./routes/authRoutes')(app);
+require('./Routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
